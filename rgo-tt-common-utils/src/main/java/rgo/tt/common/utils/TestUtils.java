3789d66793c9ab939f0d61d762cb4baddba12a01
evgeny-mordyasov <michisig11i7@gmail.com>
2023-07-19 21:31:41 +0700

    TT-37: update utils

--- rgo-tt-common-utils/src/main/java/rgo/tt/common/utils/TestUtils.java
+++ rgo-tt-common-utils/src/main/java/rgo/tt/common/utils/TestUtils.java
@@ -1,6 +1,7 @@
 package rgo.tt.common.utils;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.List;
 
 public final class TestUtils {
@@ -10,9 +11,13 @@ public final class TestUtils {
 
     public static <T> void assertNullFields(T object, List<String> nonEmptyFields) throws IllegalAccessException {
         for (Field field : object.getClass().getDeclaredFields()) {
-            if (nonEmptyFields.contains(field.getName())) continue;
+            if (nonEmptyFields.contains(field.getName()) || Modifier.isStatic(field.getModifiers())) continue;
+
             field.setAccessible(true);
-            if (field.get(object) != null) throw new IllegalStateException("The field={" + field + "} should be null.");
+
+            if (field.get(object) != null) {
+                throw new IllegalStateException("The field={" + field + "} should be null.");
+            }
         }
     }
 }
