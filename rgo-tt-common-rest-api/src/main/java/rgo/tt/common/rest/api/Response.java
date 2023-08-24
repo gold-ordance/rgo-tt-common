71a0b1cbae55b4bc9619160bae56c4defcd6a2b4
evgeny-mordyasov <michisig11i7@gmail.com>
2023-08-24 18:20:25 +0700

    add static method for Response class

--- rgo-tt-common-rest-api/src/main/java/rgo/tt/common/rest/api/Response.java
+++ rgo-tt-common-rest-api/src/main/java/rgo/tt/common/rest/api/Response.java
@@ -1,6 +1,15 @@
 package rgo.tt.common.rest.api;
 
+import static rgo.tt.common.rest.api.ErrorResponse.notFound;
+import static rgo.tt.common.rest.api.SuccessResponse.noContent;
+
 public interface Response {
 
     Status getStatus();
+
+    static Response from(boolean isDeleted) {
+        return isDeleted
+                ? noContent()
+                : notFound();
+    }
 }
