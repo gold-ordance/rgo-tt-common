419739f4c89a5127561e4bf5ef12d2c6889aae64
evgeny-mordyasov <michisig11i7@gmail.com>
2023-08-25 22:38:05 +0700

    add getFirstSymbol method to HelperUtils

--- rgo-tt-common-utils/src/main/java/rgo/tt/common/utils/HelperUtils.java
+++ rgo-tt-common-utils/src/main/java/rgo/tt/common/utils/HelperUtils.java
@@ -2,11 +2,20 @@ package rgo.tt.common.utils;
 
 public final class HelperUtils {
 
+    private static final int FIRST_SYMBOL_INDEX = 0;
+
     private HelperUtils() {
     }
 
     public static String size(String str) {
         if (str == null) return null;
         return "{size=" + str.length() + "}";
+    }
+
+    public static String getFirstSymbol(String str) {
+        if (str == null) return null;
+        if (str.isEmpty()) return "";
+        return String.valueOf(
+                str.charAt(FIRST_SYMBOL_INDEX));
     }
 }
