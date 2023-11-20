992b61fc700518b8d70b785ebb396eb2b242ed40
evgeny-mordyasov <michisig11i7@gmail.com>
2023-11-20 21:45:13 +0700

    rename startServerWithService to startArmeriaServer

--- rgo-tt-common-armeria/src/main/java/rgo/tt/common/armeria/test/simpleserver/ArmeriaServerManager.java
+++ rgo-tt-common-armeria/src/main/java/rgo/tt/common/armeria/test/simpleserver/ArmeriaServerManager.java
@@ -2,17 +2,27 @@ package rgo.tt.common.armeria.test.simpleserver;
 
 public final class ArmeriaServerManager {
 
-    private static final int PORT = 8090;
-    private static final String HOST = "http://127.0.0.1:" + PORT;
+    private static final int DEFAULT_PORT = 8090;
+    private static final String DEFAULT_HOST = "http://127.0.0.1:" + DEFAULT_PORT;
 
     private static SimpleArmeriaServer server;
+    private static String host;
 
     private ArmeriaServerManager() {
     }
 
-    public static void startServerWithService(Object restService) {
+    public static void startArmeriaServer(Object restService) {
         if (server == null) {
-            server = new SimpleArmeriaServer(PORT, restService);
+            host = DEFAULT_HOST;
+            server = new SimpleArmeriaServer(DEFAULT_PORT, restService);
+            server.start();
+        }
+    }
+
+    public static void startArmeriaServer(int port, Object restService) {
+        if (server == null) {
+            host = "http://127.0.0.1:" + port;
+            server = new SimpleArmeriaServer(port, restService);
             server.start();
         }
     }
@@ -25,6 +35,6 @@ public final class ArmeriaServerManager {
     }
 
     public static String getHost() {
-        return HOST;
+        return host;
     }
 }
